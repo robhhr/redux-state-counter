129d2f84e94fbc4c1b81e88f6349ed1537d9bf28
Roberto H <robhhr@outlook.com>
2019-11-17 21:56:52 -0800

    add and style card components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
+import { Global, css } from "@emotion/core";
 import "./App.css";
+import Card from "./components/Card";
 
-function App() {
-  return <p>heyoooooooo</p>;
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Global
+          styles={css`
+            * {
+              box-sizing: border-box;
+            }
+          `}
+        />
+        <Card />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { css } from "@emotion/core";
+import { jsx } from "@emotion/core";
+import Title from "./Title";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <Title title={"Hallo test"} />
+    </div>
+  );
+};
+
+export default Card;
