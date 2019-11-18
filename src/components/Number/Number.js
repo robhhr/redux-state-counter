36f2b7b647e6e7ef4b04288f408b2b2f6fd35fe4
Roberto H <robhhr@outlook.com>
2019-11-18 01:27:52 -0800

    style buttons

--- src/components/Number/Number.js
+++ src/components/Number/Number.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Number.css";
+import { isS } from "xmlchars/xml/1.0/ed5";
 
 const Number = () => {
   const [number, isNumber] = useState(0);
@@ -7,9 +8,19 @@ const Number = () => {
     <div className="number-container">
       <p>{number}</p>
       <div className="buttons">
-        <button onClick={() => isNumber(number - 1)}>-</button>
-        <button onClick={() => isNumber(0)}>RESET</button>
-        <button onClick={() => isNumber(number + 1)}>+</button>
+        <button
+          onClick={() => {
+            isNumber(number - 1) || console.log("decrease");
+          }}
+        >
+          -
+        </button>
+        <button onClick={() => isNumber(0) || console.log("reset")}>
+          RESET
+        </button>
+        <button onClick={() => isNumber(number + 1) || console.log("increase")}>
+          +
+        </button>
       </div>
     </div>
   );
