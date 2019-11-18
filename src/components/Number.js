c961d190b9b60bd3dde952f1b776916165f46e33
Roberto H <robhhr@outlook.com>
2019-11-17 23:34:28 -0800

    state hook reset counter

--- src/components/Number.js
+++ src/components/Number.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 
 const Number = () => {
   const [number, isNumber] = useState(0);
-
   return (
-    <>
+    <div className="number-container">
       <p>{number}</p>
       <button onClick={() => isNumber(number + 1)}>+</button>
+      <button onClick={() => isNumber(0)}>RESET</button>
       <button onClick={() => isNumber(number - 1)}>-</button>
-    </>
+    </div>
   );
 };
 
